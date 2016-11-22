1b9d7caed6f5f709678e7645201d6db8fc60b3ef
Bror Winther <brorwinther@gmail.com>
2016-11-22 02:13:48 +0100

    hello world example of spring

--- Server/src/test/java/com/server/Greeting.java
+++
@@ -1,7 +0,0 @@
-package com.server;
-
-/**
- * Created by noko on 11/21/16.
- */
-public class Greeting {
-}
--- project/server/src/main/java/com/server/GreetingController.java
+++ project/server/src/main/java/com/server/GreetingController.java
@@ -4,10 +4,21 @@ package com.server;
  * Created by noko on 11/22/16.
  */
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(){
+        return new Greeting(1, "Hello, World");
+    }
+
+
 
 }
